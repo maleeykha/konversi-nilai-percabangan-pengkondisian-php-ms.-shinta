9d885396361a17b00d37be4f85857951f345295a
L <maleeykha17927@gmail.com>
2026-08-21 15:18:29 +0700

    Create cekusia.php

---
+++ cekusia.php
@@ -0,0 +1,13 @@
+?php
+
+$usia = 20;
+
+if ($usia < 13) {
+    echo "Anak-anak";
+} elseif ($usia <= 17) {
+    echo "Remaja";
+} elseif ($usia <= 59) {
+    echo "Dewasa";
+} else {
+    echo "Lansia";
+}
